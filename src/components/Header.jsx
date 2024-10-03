2dad004aa88223867fb8407132794f69a39e6530
ludoRap33 <ludovicdufaure@gmail.com>
2024-10-03 15:49:29 +0200

    two step

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Link, NavLink } from "react-router-dom"
+import logo from '../assets/logo.png'
+
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <Link to="/">
+                    <img src={logo} alt="Company Logo"></img>
+                </Link>
+                <ul>
+                    <li>
+                        <NavLink to={'/'}>Accueil</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/about'}>A propos</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
