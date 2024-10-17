a8d80dff23700b4c43333882d39a76c8ae9aebdc
ludoRap33 <ludovicdufaure@gmail.com>
2024-10-17 23:32:05 +0200

    fourteen step

--- src/pages/Lodging.jsx
+++ src/pages/Lodging.jsx
@@ -6,7 +6,17 @@ const Lodging = () => {
 
     return (
         <section id='lodging'>
+
             {id}
+            <div id="banner">
+			<img class="banner-img" src="./assets/images/slideshow/slide1.jpg" alt="Banner Print-it">
+			<img class="arrow arrow_left" src="./assets/images/arrow_left.png" alt="arrow left">
+			<img id="arrow_right" class="arrow arrow_right" src="./assets/images/arrow_right.png" alt="arrow right">
+			<div class="dots">
+			</div>
+            
+		</div>
+
         </section>
     )
 }
