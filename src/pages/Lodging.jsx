13f20d584809ef7abcf47460045e35d31552c630
ludoRap33 <ludovicdufaure@gmail.com>
2024-10-18 15:30:09 +0200

    fiveteen step

--- src/pages/Lodging.jsx
+++ src/pages/Lodging.jsx
@@ -8,13 +8,19 @@ const Lodging = () => {
         <section id='lodging'>
 
             {id}
-            <div id="banner">
+        <header class="container">
+		    <h1>
+			<img class="image" src="./packagge-lock.png" alt="Print it ! Impressions d'entreprises">
+
+		</header>
+		</div>
+
+        <div id="banner">
 			<img class="banner-img" src="./assets/images/slideshow/slide1.jpg" alt="Banner Print-it">
 			<img class="arrow arrow_left" src="./assets/images/arrow_left.png" alt="arrow left">
 			<img id="arrow_right" class="arrow arrow_right" src="./assets/images/arrow_right.png" alt="arrow right">
 			<div class="dots">
 			</div>
-            
 		</div>
 
         </section>
